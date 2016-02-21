77b9897590bd1ab14fc12715de54e9f61ede195f
Cristian Opincaru <cristian.opincaru@gmail.com>
2016-02-21 09:15:51 +0200

    something

--- src/main/java/sample/plugin/App.java
+++ src/main/java/sample/plugin/App.java
@@ -1,10 +1,19 @@
 package sample.plugin;
 
-import org.antlr.stringtemplate.StringTemplate;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.stringtemplate.v4.ST;
 
 /**
  * Hello world!
@@ -22,18 +31,57 @@ public class App extends AbstractMojo {
 	@Parameter(property="count")
 	private int testCount;
 	
+	@Parameter(property="basePackage", defaultValue="org.thales.tato")
+	private String basePackage;
+	
+	@Parameter(property="testGroup", defaultValue="weiche")
+	private String testGroup;
+	
+	private final static String TEST_TEMPLATE = "TestTemplate.java";
+		
 	public void execute() throws MojoExecutionException {
         getLog().info( "SourceFolder: " + sourceFolder);
         getLog().info( "ChapterName: " + chapterName);
         getLog().info( "TestCount: " + testCount);
         
-        for (int i=1; i<=testCount; i++) 
-        	generateTestCaseTemplate(chapterName, i);
+        for (int i=1; i<=testCount; i++) {
+			try {
+				generateTestCaseTemplate(i);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
     }
 	
-	private void generateTestCaseTemplate(String chapterName, int id) {
-		// read file name
-		StringTemplate template = new StringTemplate();
+	private void generateTestCaseTemplate(int id) throws IOException {
+		// Read class template
+		String line;
+		StringBuffer template = new StringBuffer();
+		InputStream is = this.getClass().getClassLoader().getResourceAsStream(TEST_TEMPLATE);
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		while ((line = br.readLine()) != null) {
+			template.append(line);
+		}	
 		
+		ST st = new ST(template.toString());
+		
+		String className = "Test" + chapterName.replace(".", "_") + "_" + id;
+		st.add("className", className);
+		
+		String packageName = basePackage + "." + testGroup + ".c" + chapterName.replace(".", "_");
+		st.add("packageName", packageName);
+		
+		String testId = chapterName + "." + id;
+		st.add("testId", testId);
+		
+		System.out.println(st.render());
+		
+		// Write class file
+		String fileName = sourceFolder + File.separator + basePackage.replace(".", File.separator) + File.separator + 
+				testGroup + File.separator + "c" + chapterName.replace(".", "_") + File.separator + className + ".java";
+		System.out.println("FileName: " + fileName);
+		
+		// BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
 	}
 }
--- src/main/resources/TestTemplate.java
+++ src/main/resources/TestTemplate.java
@@ -1,10 +1,11 @@
-package sample.plugin;
+package <packageName>;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-public class Test<id> {
+public class <className> {
 	
+	@id("<testId>")
 	@Test
 	public void execute() throws Exception {
 		Assert.fail("test case not implemeted");
